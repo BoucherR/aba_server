ac5eb380743bd208de28fbcc131746aff7d24314
Ryan <Ryan.Boucher95@gmail.com>
2021-09-16 18:29:14 -0400

    More PORT fixes

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	_ "database/sql"
+	"fmt"
 	"os"
 
 	"github.com/BoucherR/aba_server/db"
@@ -18,5 +19,6 @@ func main() {
 	r := router.SetupRouter()
 	// Listen and Serve in 0.0.0.0:8081
 	// r.Run(":8081")
+	fmt.Printf("STARTING ON PORT: %v", os.Getenv("PORT"))
 	r.Run(os.Getenv("PORT"))
 }
