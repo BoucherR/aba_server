64aeb0e97b68579ccfc98be6059212dcb64cb675
Ryan <Ryan.Boucher95@gmail.com>
2021-09-16 18:25:33 -0400

    Adding a PORT environmental variable for Heroku

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	_ "database/sql"
+	"os"
 
 	"github.com/BoucherR/aba_server/db"
 	"github.com/BoucherR/aba_server/router"
@@ -16,5 +17,6 @@ func init() {
 func main() {
 	r := router.SetupRouter()
 	// Listen and Serve in 0.0.0.0:8081
-	r.Run(":8081")
+	// r.Run(":8081")
+	r.Run(os.Getenv("PORT"))
 }
